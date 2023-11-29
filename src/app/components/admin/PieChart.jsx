1183993805305e6a93fd435ddd8880d0070745c3
Gabriel Torres <Gabrieltorres2014@gmail.com>
2023-11-28 22:03:46 -0400

    pie chart

--- src/app/components/admin/PieChart.jsx
+++ src/app/components/admin/PieChart.jsx
@@ -7,35 +7,21 @@ import { useEffect, useState } from 'react';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  labels:  ['Cargando los votos...'] ,
   color: '#FFF',
   datasets: [
     {
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
+      data: [1],
+      backgroundColor: ['#48ad86'],
+      borderColor:  ['#48ad86'],
       borderWidth: 1,
       color: '#FFF'
     },
   ]
 };
 
-const options = {
+const options = {#ffffff
     plugins: {
         legend: {
             position: 'bottom',
